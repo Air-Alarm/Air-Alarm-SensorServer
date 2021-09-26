5925ff13d55fff8e5e9c3f608d62f2c0b08cc11f
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2021-09-27 01:57:20 +0900

    weather sujung

--- main.py
+++ main.py
@@ -3,26 +3,41 @@ from flask import Flask, jsonify
 from DB import getFromDB
 
 api = Flask(__name__)
+api.config['JSON_AS_ASCII'] = False
 
 
 @api.route('/get', methods=['GET', 'POST'])
 def get():
     rows = getFromDB.getFrom("tonow")
+    temp = int(rows[-1][4])
+    temp2 = int(rows[-1][5])
     jsonDic = {"id": rows[-1][0], "time": rows[-1][1], "temperature": rows[-1][2], "humidity": rows[-1][3],
-               "dust": rows[-1][4], "CO2": rows[-1][5]}
+               "dust": temp, "CO2": temp2}
     return jsonify(jsonDic)
 
 @api.route('/getall', methods=['GET', 'POST'])
 def getall():
     t = getFromDB.getFrom("tonow")
-    return jsonify(t)
+    jsonArr = []
+    
+    for i in range(len(t)):
+        temp = int(t[i][4])
+        temp2 = int(t[i][5])
+        jsonArr.append({
+            "id": t[i][0], "time": t[i][1], "temperature": t[i][2],
+            "humidity": t[i][3], "dust": temp, "CO2": temp2
+        })
+    
+    return jsonify(jsonArr)
 
 
 @api.route('/day', methods=['GET', 'POST'])
 def day():
     rows = getFromDB.getFrom("today")
+    temp = int(rows[-1][4])
+    temp2 = int(rows[-1][5])
     jsonDic = {"id": rows[-1][0], "time": rows[-1][1], "temperature": rows[-1][2], "humidity": rows[-1][3],
-               "dust": rows[-1][4], "CO2": rows[-1][5]}
+               "dust": temp, "CO2": temp2}
     return jsonify(jsonDic)
 
 @api.route('/dayall', methods=['GET', 'POST'])
@@ -31,9 +46,11 @@ def dayall():
     jsonArr = []
     
     for i in range(len(t)):
+        temp = int(t[i][4])
+        temp2 = int(t[i][5])
         jsonArr.append({
             "id": t[i][0], "time": t[i][1], "temperature": t[i][2],
-            "humidity": t[i][3], "dust": t[i][4], "CO2": t[i][5]
+            "humidity": t[i][3], "dust": temp, "CO2": temp2
         })
     
     return jsonify(jsonArr)
@@ -42,8 +59,10 @@ def dayall():
 @api.route('/hour', methods=['GET', 'POST'])
 def hour():
     rows = getFromDB.getFrom("tohour")
+    temp = int(rows[-1][4])
+    temp2 = int(rows[-1][5])
     jsonDic = {"id": rows[-1][0], "time": rows[-1][1], "temperature": rows[-1][2], "humidity": rows[-1][3],
-               "dust": rows[-1][4], "CO2": rows[-1][5]}
+               "dust": temp, "CO2": temp2}
     return jsonify(jsonDic)
 
 @api.route('/hourall', methods=['GET', 'POST'])
@@ -53,9 +72,11 @@ def hourall():
     jsonArr = []
     
     for i in range(len(t)):
+        temp = int(t[i][4])
+        temp2 = int(t[i][5])
         jsonArr.append({
             "id": t[i][0], "time": t[i][1], "temperature": t[i][2],
-            "humidity": t[i][3], "dust": t[i][4], "CO2": t[i][5]
+            "humidity": t[i][3], "dust": temp, "CO2": temp2
         })
     
     return jsonify(jsonArr)
@@ -64,20 +85,36 @@ def hourall():
 @api.route('/weather', methods=['GET', 'POST'])
 def weather():
     rows = getFromDB.getFrom("tolocal")
-    jsonDic = {"id": rows[-1][0], "Location": rows[-1][1], "dust10": rows[-1][2], "dust25": rows[-1][3],
-               "humidity": rows[-1][4], "temperature": rows[-1][5]}
+    temp = int(rows[-1][3])
+    temp2 = int(rows[-1][4])
+    jsonDic = {"id": rows[-1][0], "Location": rows[-1][1], "time": rows[-1][2], "dust10": temp, "dust25": temp2,
+               "humidity": rows[-1][5], "temperature": rows[-1][6]}
     return jsonify(jsonDic)
 
 @api.route('/weatherall', methods=['GET', 'POST'])
 def weatherall():
     t = getFromDB.getFrom("tolocal")
     #temp = '측정장소:' + t[0] + '<br>' + '측정시간:' + t[1] + '<br>' + '미세먼지농도:' + t[2] + '㎍/m³' + '<br>' + '초미세먼지농도:' + t[3] + '㎍/m³'
-    return jsonify(t)
+    jsonArr = []
+    
+    for i in range(len(t)):
+        print(t[i][3], t[i][4])
+        try:
+            temp = int(t[i][3])
+            temp2 = int(t[i][4])
+        except Exception as e:
+            temp = 0
+            temp2 = 0
+        jsonArr.append({
+            "id": t[i][0], "Location": t[i][1], "time": t[i][2],
+            "dust10": temp, "dust25": temp2, "humidity": t[i][5], "temperature": t[i][6]
+        })
+    
+    return jsonify(jsonArr)
 
 
 if __name__ == '__main__':
     api.run(debug=True, host='0.0.0.0', port=5000)
-    api.config['JSON_AS_ASCII'] = False
 
 # 주의! 이거 끄면 서버 꺼짐. 건들지 말기
 
