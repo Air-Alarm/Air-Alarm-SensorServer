3e7a36de9d84cb5e8344ec992c30dcc6568badb6
SonChangHa <49190602+SonChangHa@users.noreply.github.com>
2021-10-04 19:48:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,36 @@ from DB import getFromDB
 api = Flask(__name__)
 api.config['JSON_AS_ASCII'] = False
 
+"""
+모든 페이지는 뒤에 all 붙일시 DB에 쌓인 모든 값 볼 수 있음.
+ex) /get은 최신 1개 센서값, /getall은 쌓여있는 모든 센서값
+"""
 
+def changeToJson(DB):
+    rows = getFromDB.getFrom(f"to{DB}")
+    temp = int(rows[-1][4])  # 소수 값 정수화
+    temp2 = int(rows[-1][5])  # 소수 값 정수화
+    jsonDic = {"id": rows[-1][0], "time": rows[-1][1], "temperature": rows[-1][2], "humidity": rows[-1][3],
+               "dust": temp, "CO2": temp2}
+    return jsonDic
+
+def changeToJsonAll(DB):
+    t = getFromDB.getFrom(f"to{DB}")
+    jsonArr = []
+    for i in range(len(t)):
+        temp = int(t[i][4])
+        temp2 = int(t[i][5])
+        jsonArr.append({
+            "id": t[i][0], "time": t[i][1], "temperature": t[i][2],
+            "humidity": t[i][3], "dust": temp, "CO2": temp2
+        })
+    return jsonArr
+
+
+# 실시간 센서 값
 @api.route('/get', methods=['GET', 'POST'])
 def get():
+<<<<<<< HEAD
     rows = getFromDB.getFrom("tonow")
     temp = int(rows[-1][4])
     temp2 = int(rows[-1][5])
@@ -28,10 +55,18 @@ def getall():
         })
 
     return jsonify(jsonArr)
+=======
+    return jsonify(changeToJson("now"))
+
+@api.route('/getall', methods=['GET', 'POST'])
+def getall():
+    return jsonify(changeToJsonAll("now"))
+>>>>>>> 6a2b75715ae1db1d81465c43660bbe9b98bb166c
 
 
 @api.route('/day', methods=['GET', 'POST'])
 def day():
+<<<<<<< HEAD
     rows = getFromDB.getFrom("today")
     temp = int(rows[-1][4])
     temp2 = int(rows[-1][5])
@@ -53,10 +88,18 @@ def dayall():
         })
 
     return jsonify(jsonArr)
+=======
+    return jsonify(changeToJson("day"))
+
+@api.route('/dayall', methods=['GET', 'POST'])
+def dayall():
+    return jsonify(changeToJsonAll("day"))
+>>>>>>> 6a2b75715ae1db1d81465c43660bbe9b98bb166c
 
 
 @api.route('/hour', methods=['GET', 'POST'])
 def hour():
+<<<<<<< HEAD
     rows = getFromDB.getFrom("tohour")
     temp = int(rows[-1][4])
     temp2 = int(rows[-1][5])
@@ -79,12 +122,25 @@ def hourall():
         })
 
     return jsonify(jsonArr)
+=======
+    return jsonify(changeToJson("hour"))
+
+@api.route('/hourall', methods=['GET', 'POST'])
+def hourall():
+    return jsonify(changeToJsonAll("hour"))
+>>>>>>> 6a2b75715ae1db1d81465c43660bbe9b98bb166c
 
 
 @api.route('/weather', methods=['GET', 'POST'])
 def weather():
     rows = getFromDB.getFrom("tolocal")
+<<<<<<< HEAD
     jsonDic = {"id": rows[-1][0], "Location": rows[-1][1], "time": rows[-1][2], "dust10": rows[-1][3], "dust25": rows[-1][4],
+=======
+    temp = int(rows[-1][3])
+    temp2 = int(rows[-1][4])
+    jsonDic = {"id": rows[-1][0], "Location": rows[-1][1], "time": rows[-1][2], "dust10": temp, "dust25": temp2,
+>>>>>>> 6a2b75715ae1db1d81465c43660bbe9b98bb166c
                "humidity": rows[-1][5], "temperature": rows[-1][6]}
     return jsonify(jsonDic)
 
@@ -92,7 +148,22 @@ def weather():
 def weatherall():
     t = getFromDB.getFrom("tolocal")
     #temp = '측정장소:' + t[0] + '<br>' + '측정시간:' + t[1] + '<br>' + '미세먼지농도:' + t[2] + '㎍/m³' + '<br>' + '초미세먼지농도:' + t[3] + '㎍/m³'
-    return jsonify(t)
+    jsonArr = []
+    
+    for i in range(len(t)):
+        print(t[i][3], t[i][4])
+        try:
+            temp = int(t[i][3])
+            temp2 = int(t[i][4])
+        except Exception as e:
+            temp = 0
+            temp2 = 0
+        jsonArr.append({
+            "id": t[i][0], "Location": t[i][1], "time": t[i][2],
+            "dust10": temp, "dust25": temp2, "humidity": t[i][5], "temperature": t[i][6]
+        })
+    
+    return jsonify(jsonArr)
 
 
 if __name__ == '__main__':
